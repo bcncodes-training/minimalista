128cbd26194ff1c5534ec52290025ee11ec80fa1
Roberto Garcia <41040915+naranjito72@users.noreply.github.com>
2020-02-28 12:06:25 +0100

    Add files via upload
    Not using regex

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-    module.exports = function minimalista(string) {
-      if (typeof string !== "string") throw new TypeError
-      ("Minimalista requiere un string!");
+    /* module.exports =  */function minimalista(string) {
+      if (typeof string !== "string") /* throw new TypeError */
+      return ("Minimalista requiere un string!");
       return string.replace(/\s/g, "");
     };
+console.log(minimalista(['Hello World']));
+
+ const minimalista1 = (str) =>{
+if(typeof str !== 'string') /* throw new TypeError */ 
+  return ("Minimalista requiere un string");
+    return str.split(' ').join('').trim();
+    };
+console.log(minimalista1('Hello W   or  l   d   '));
